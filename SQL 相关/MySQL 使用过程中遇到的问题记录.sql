ec4c82348dbc1081740818bf176e8ce10d479fe1
whxcer <whxcer@outlook.com>
2020-06-10 23:12:28 +0800

    使用 MySQL 做 LeetCode 习题过程中遇到的问题记录

--- SQL 相关/MySQL 使用过程中遇到的问题记录.sql
+++ SQL 相关/MySQL 使用过程中遇到的问题记录.sql
@@ -82,3 +82,108 @@
 8、IFNULL 函数
     IFNULL() 函数用于判断第一个表达式是否为 NULL，如果为 NULL 则返回第二个参数的值，如果不为 NULL 则返回第一个参数的值。
     IFNULL(expression, alt_value)
+
+
+9、取余
+    select MOD(5, 2);        -- 1
+    select 5 mod 2;          -- 1
+    select 5 % 2;            -- 1
+
+
+10、IF 函数
+    IF(expr1, expr2, expr3)，如果 expr1 的值为 true，则返回 expr2 的值，如果 expr1 的值为 false，则返回expr3的值。
+
+
+11、多个字段的 in 或者是 not in
+    前面的字段彼此相关联，同时存在才算是真的在
+    (a, b) in (select a, b from table)
+    (a, b, c) in (select a, b, c from table)
+
+
+12、delete 的新解法
+    DELETE
+        p1
+    FROM
+        Person p1, Person p2
+    WHERE
+        p1.Email = p2.Email AND p1.Id > p2.Id
+
+
+13、mysql 中 limit 后面不能带运算符，只能是常量
+    limit 里面不可以进行 加减乘除 运算
+
+
+14、 mysql 中定义变量的两种方式：
+    使用 set ，变量名以 @ 开头
+        set @var=1;
+    使用 declare
+        declare var1 int default 0;
+        set var1 = 10;
+
+
+15、 日期类型可以使用 大于、小于 运算符直接进行比较运算
+    select * from students where birth > '1990-01-01'
+
+
+16、现在 MySQL 有函数支持排名
+    dense_rank()：连续排序           -- 1 2 2 3
+        DENSE_RANK() OVER (
+            PARTITION BY <expression>[{,<expression>...}]
+            ORDER BY <expression> [ASC|DESC], [{,<expression>...}]
+        )
+
+        首先，PARTITION BY子句将FROM子句生成的结果集划分为分区。DENSE_RANK()函数应用于每个分区。
+        其次，ORDER BY  子句指定DENSE_RANK()函数操作的每个分区中的行顺序。
+
+    rank()：跳跃排序                 -- 1 2 2 4
+        RANK() OVER (
+            PARTITION BY <expression>[{,<expression>...}]
+            ORDER BY <expression> [ASC|DESC], [{,<expression>...}]
+        )
+
+        首先，PARTITION BY子句将结果集划分为分区。RANK()功能在分区内执行，并在跨越分区边界时重新初始化。
+        其次，ORDER BY子句按一个或多个列或表达式对分区内的行进行排序。
+
+
+17、:=和=的区别
+    =
+        只有在set和update时才是和:=一样，赋值的作用，其它都是等于的作用。
+    :=
+        不只在set和update时时赋值的作用，在select也是赋值的作用。
+
+    为了避免多添加一个输出列，而用 @i=(@i:=2)。先获取 @i 的值替换 @i，然后再将 2 赋值给 @i，因此可以直接比较
+        select if(@i=(@i:=2), 0, 1) from (select @i:=1) as a;
+
+
+18、字符串转成数字
+    使用函数
+        CAST(value as type);
+        CONVERT(value, type);
+        type 类型： 浮点数 : DECIMAL  整数 : SIGNED  无符号整数 : UNSIGNED  日期 : DATE  时间: TIME  日期时间型 : DATETIME 等等。
+
+            select * from scores order by CAST(course_id as SIGNED)  desc
+            select * from scores order by CONVERT(course_id, SIGNED)  desc
+
+
+19、SQL 日期类型可以使用大于、小于号直接进行比较运算
+    select * from students where birth > '1990-01-01'
+    select * from students where birth > CONVERT('1990-1-1', DATETIME);
+    select * from students where birth > CAST('1990-1-1' as DATETIME);
+
+20、MySQL 的 delete not in 操作
+    正确：delete from tableA where id not in  (1, 2, 3);
+    正确：delete from tableA where id not in (select id from tableB)
+
+    错误：      delete from tableA where id not in (select MIN(id) from tableA group by email)
+    解决办法：  delete from tableA where id not in (select * from (select MIN(id) from tableA group by email) as t)
+
+
+21、使用 SQL 保留字做输出列名
+    使用 SQL 的保留字作为输出列名，则会报错，但是你依旧想要使用该字符作为输出列名，则将其加上两个单引号括起来即可
+
+
+22、group by 使用过程中的问题
+    使用 GROUP BY 可以为每个元素创建一组结果行，仅允许针对多个值（例如SUM，AVG，MIN，MAX，COUNT）进行聚合操作。
+    单独地使用 group by (不加聚合函数)，每次只能显示出每组记录的第一条记录。
+    如果想要对其中该元素都某一属性进行筛选，则需要添加聚合函数（MAX 或 SUM）---单个元素属性的聚合函数 （单个元素属性的聚合函数 == 属性本身）
+    因此但凡使用 group by，需要挑选值时，前面一定要有聚合函数（MAX /MIN / SUM /AVG / COUNT）
