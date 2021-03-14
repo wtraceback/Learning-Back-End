2b5898ae63c8cbea0653c3b26691b0d87be27741
wtraceback <whxcer@outlook.com>
2021-03-14 10:15:46 +0800

    Markdown 学习笔记

---
+++ Markdown 学习笔记.py
@@ -0,0 +1,66 @@
+Markdown 是一种轻量级的「标记语言」，它的基本语法非常简明，常用的标记符号大致十来个，因此学习成本很低，而且当你熟悉了这种语法规则，那么将有一劳永逸的效果。
+
+### Markdown 的优点
+1. 语法简明、学习成本低，学成后将终身受用
+2. 纯文本内容，能保证在任何平台，打开来都是同样的格式
+3. 代码高亮
+
+### Markdown 语法
+ 1. **标题**
+    用法：在文字前面加 # 号
+![标题](https://upload-images.jianshu.io/upload_images/17731575-387eabf0e6c108b5.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
+
+2. **斜体**
+用法：在文字前后分别添加一个 * 号
+![斜体](https://upload-images.jianshu.io/upload_images/17731575-7828651d8824b97e.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
+
+3. **粗体**
+用法：在文字前后分别添加两个 * 号
+![粗体](https://upload-images.jianshu.io/upload_images/17731575-f0862c6f7ed53993.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
+
+4. **分割线**
+用法：只需要在空白行连续输入三个 * 号
+![分割线](https://upload-images.jianshu.io/upload_images/17731575-86483759acb12214.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
+
+5. **列表**
+列表分为有序列表和无序列表
+- 有序列表
+用法：在文字前面加 1. 2. 3. 符号并且空一格
+- 无序列表
+用法：在文字的前面加上 - 或 * 符号并且空一格
+![列表](https://upload-images.jianshu.io/upload_images/17731575-3601a751cfe86a41.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
+
+6. **引用**
+如果你需要引用一段别处的句子，那么就要用引用的格式
+用法：在文字前面加 > 尖括号并且空一格
+![引用](https://upload-images.jianshu.io/upload_images/17731575-e1029f73e6da7bbb.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
+
+7. **链接**
+在文章中插入链接
+用法：\[文字](链接)
+![插入链接](https://upload-images.jianshu.io/upload_images/17731575-b650a32a3f4122c3.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
+
+8. **图片**
+插入图片和插入链接的方式类似，不过插入图片比插入链接多了一个 ! 号
+用法：\!\[文字](链接)
+![插入图片](https://upload-images.jianshu.io/upload_images/17731575-d220655903d36d02.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
+
+9. **表格**
+表格的语法比较麻烦，就不详细说明了，如下图所示
+![表格](https://upload-images.jianshu.io/upload_images/17731575-db850107a6d4fed2.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
+
+10. **代码框**
+用法：使用三个反引号 ` 将代码包起来(反引号的按键是左上角 Esc 键下方的 ~ 键)
+![代码框](https://upload-images.jianshu.io/upload_images/17731575-84c7bbd460d693b8.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
+
+11. **显示转义字符**
+用法：在 MarkDown 编辑器中，如果想要让 \```、\#、\* 等符号原样显示，不被解析成对应的 Markdown 语法，则可以使用反斜杠 **\\** 来进行转义
+![显示转义字符](https://upload-images.jianshu.io/upload_images/17731575-47e919842ecfdc6a.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
+
+12. **更改文字的颜色**
+用法：\$\color{颜色}{文字}$
+颜色的表示方法：颜色的关键词、十六进制值、rgb
+![文字的颜色](https://upload-images.jianshu.io/upload_images/17731575-74f5303da4905f15.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
+
+### 总结
+以上的 Markdown 语法在日常的使用中基本足够了，只要多加练习，配合好用的工具，写起东西来肯定会行云流水。
